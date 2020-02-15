8f09c7e33d9583e8913948a88cf0c0a784fbd6ed
abstractpaper <aziz.alfoudari@gmail.com>
2020-02-16 01:00:41 +0300

    Added Retry function with tests.

---
+++ funcion_test.go
@@ -0,0 +1,53 @@
+package swissarmy
+
+import (
+	"errors"
+	"testing"
+	"time"
+
+	"github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus/hooks/test"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRetrySuccess(t *testing.T) {
+	f := func() error {
+		_ = "x"
+		return nil
+	}
+
+	test := test.NewGlobal()
+
+	Retry(f, nil)
+
+	entries := test.AllEntries()
+	assert.Equal(t, 0, len(entries))
+}
+
+func TestRetryError(t *testing.T) {
+	var t1, t2 time.Time
+	f := func() error {
+		_ = "x"
+		t1 = time.Now()
+
+		if t2.After(t1) {
+			return errors.New("test")
+		}
+
+		return nil
+	}
+
+	test := test.NewGlobal()
+
+	t2 = time.Now().Add(5 * time.Second)
+	Retry(f, nil)
+
+	entries := test.AllEntries()
+	errorCount := 0
+	for _, e := range entries {
+		if e.Level == logrus.ErrorLevel {
+			errorCount = errorCount + 1
+		}
+	}
+	assert.Equal(t, 2, errorCount)
+}
---
+++ function.go
@@ -0,0 +1,46 @@
+package swissarmy
+
+import (
+	"os"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+)
+
+// Retry calls f() until successful, if it fails then it waits
+// for a period that starts with 2 seconds and increases exponentially
+// until it is capped at 1 minute.
+func Retry(f func() error, interrupt chan os.Signal) {
+	var sleep time.Duration = 2
+	var retryTimestamp time.Time = time.Now()
+
+	for {
+		select {
+		case <-interrupt:
+			log.Warn("Interrupt/kill signal received, quitting.")
+			os.Exit(1)
+		default:
+			// call f() if the current time passed retryTimestamp
+			if time.Now().After(retryTimestamp) {
+				err := f()
+				if err != nil {
+					log.Error(err)
+
+					// couldn't connect, retry.
+					log.Infof("Retrying in %d seconds", sleep)
+					retryTimestamp = time.Now().Add(sleep * time.Second)
+
+					// increase exponentially, cap at ~ 1 minute (64 seconds).
+					if sleep < 64 {
+						sleep = sleep * 2
+					}
+				} else {
+					return
+				}
+			}
+
+			// loop every second, check for signals and timeline in each iteration.
+			time.Sleep(1 * time.Second)
+		}
+	}
+}
